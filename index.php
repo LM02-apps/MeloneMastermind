a3a82a8a3f686accbae18dc819e270f9dc952780
LM02-apps <lorenzoita29@gmail.com>
2021-10-06 09:48:28 +0200

    1st commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+
+    </head>
+    
+    <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm border">
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown button
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm border">
+                prova
+            </div>
+            <div class="col-sm border">
+                prova
+            </div>
+            <div class="col-sm border">
+            prova
+            </div>
+            <div class="col-sm border">
+            <img src="../images/spunta.gif">
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    
+    </body>
+</html>
